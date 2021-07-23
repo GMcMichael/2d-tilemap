cc798b6580f34ec601708b5b9c2dd350cde2a584
GMcMichael <gmcmichael0717@gmail.com>
2021-07-23 00:43:29 -0700

    Working on getting border perimeter for blockage map

--- Exoplorer/Assets/Scripts/World Generation/MapGeneration.cs
+++ Exoplorer/Assets/Scripts/World Generation/MapGeneration.cs
@@ -81,24 +81,33 @@ public class MapGeneration : MonoBehaviour
 
     public float[,] GenerateBlockageMap(float[,] noiseMap) {//blocked = 0, unblocked = 1            //Make the blockage map use the borders
         float[,] blockageMap = new float[noiseMap.GetLength(0), noiseMap.GetLength(1)];
-        BorderInfo leftBorder = regionBorders[0];
-        BorderInfo rightBorder = regionBorders[regionBorders.Count - 1];//go between each set of borders(stating with 0 as a border, not sure about 1) and take the perimiter and set it to blockage
 
+        //add borders at the ends (0 and 1) if they don't exist
+        if(regionBorders[0].Time != 0) regionBorders.Insert(0, new BorderInfo("Start", 0));
+        if(regionBorders[regionBorders.Count-1].Time != 1) regionBorders.Add(new BorderInfo("End", 1));
 
-        for(int i = 0; i < regionBorders.Count; i++) {
-            if(regionBorders[i].Time <= time) {
-                leftBorder = regionBorders[i];
-            }
-            if(regionBorders[i].Time >= time) {
-                rightBorder = regionBorders[i];
-                break;
+        int[,,] tempMap = new int[blockageMap.GetLength(0), blockageMap.GetLength(1), regionBorders.Count-1];
+        for (int i = 0; i < regionBorders.Count-1; i++)//go between each set of borders and take the perimiter and set it to blockage
+        {
+            float leftBorder = regionBorders[i].Time;
+            float rightBorder = regionBorders[i+1].Time;
+
+            for (int x = 0; x < noiseMap.GetLength(0); x++) {
+                for (int y = 0; y < noiseMap.GetLength(1); y++) {
+                    if(noiseMap[x,y] >= leftBorder && noiseMap[x,y] <= rightBorder) tempMap[x,y,i] = 1;//if it should be blocked, set it to 1 since default on creation is 0
+                }
             }
         }
-
-
+        
+        for (int i = 0; i < tempMap.GetLength(2); i++)
+        {
+            //all the layers are set, loop through the layers and get the perimeter. If it is part of the perimeter set blockageMap[x,y] to 1
+            //current layer is tempMap[0,0,i]
+        }
+        return blockageMap;
 
         //check each side of tile, if it is heigher then a variable height, make it a blocker
-        for (int x = 0; x < noiseMap.GetLength(0); x++) {
+        /*for (int x = 0; x < noiseMap.GetLength(0); x++) {
             for (int y = 0; y < noiseMap.GetLength(1); y++) {
                 float currHeight = noiseMap[x,y];
                 if(y-1 >= 0) {//check up
@@ -128,7 +137,7 @@ public class MapGeneration : MonoBehaviour
                 blockageMap[x,y] = 1;
             }
         }
-        return blockageMap;
+        return blockageMap;*/
     }
 
     public bool RegionsChanged() {
